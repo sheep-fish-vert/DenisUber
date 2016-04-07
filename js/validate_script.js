3ba3be87862bd126c32a3a9c1f79b647f974a366
Art Garm <artem.garmatyk@gmail.com>
2016-04-07 14:29:03 +0300

    asdfasdf

--- js/validate_script.js
+++ js/validate_script.js
@@ -98,7 +98,6 @@ function validate(form, options){
 function validationCall(form) {
 // yaMetrick
     yandexID.reachGoal(redFlag);    
-    console.log(redFlag);
   var thisForm = $(form);
   var formSur = thisForm.serialize();
 
